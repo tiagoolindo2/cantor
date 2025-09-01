0e6ecfb319d808bdd11c65fcb0f766d217a24353
tiagoolindo2 <lu.nabuco.sil@gmail.com>
2025-09-01 10:25:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,15 @@ document.addEventListener('DOMContentLoaded', function () {
         botaoDeAcessibilidade.classList.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
 
+        const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+        botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)
+
     })
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
 
     let tamanhoAtualFonte = 1;
 
@@ -24,4 +29,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+     alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
+
+
 })
